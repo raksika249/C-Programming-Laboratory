655ebeb4dce9ee6cb0dd8b4629011d8290a65da4
raksika249 <raksika.249@gmail.com>
2024-09-30 15:23:17 +0530

    Create areaOfRec.c

---
+++ areaOfRec.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int l,b,area;
+    printf("enter length:");
+    scanf("%d",&l);
+    printf("enter breadth:");
+    scanf("%d",&b);
+    area=l*b;
+    printf("Area:%d",area);
+    return 0;
+}
