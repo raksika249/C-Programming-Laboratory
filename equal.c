0e75dea2843bd4eea2cc2036c388eb9058193ef1
raksika249 <raksika.249@gmail.com>
2024-10-06 13:35:57 +0530

    Create equal.c

---
+++ equal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int a,b;
+   scanf("%d%d",&a,&b);
+   if(a==b)
+   {
+       printf("%d is equal to %d",a,b);
+   }
+   else
+   {
+       printf("%d is not equal to %d",a,b);
+   }
+    return 0;
+}
