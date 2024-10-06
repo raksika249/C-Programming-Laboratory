3ec9b5b80831a4dabfe6bbb45d440f930e5d613a
raksika249 <raksika.249@gmail.com>
2024-10-06 13:31:55 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int a,b;
+   scanf("%d%d",&a,&b);
+   if(a>b)
+   {
+       printf("largest=%d",a);
+   }
+   else
+   {
+       printf("largest=%d",b);
+   }
+    return 0;
+}
